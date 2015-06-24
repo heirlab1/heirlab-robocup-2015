f04c478d9cbb7f0d77a82b8f849482d7b4fdf4f7
Mo King <muhsinking@gmail.com>
2015-06-24 10:19:09 -0500

    Define all soccer-type-a motions in tx header

--- master_control/include/tx_serial.h
+++ master_control/include/tx_serial.h
@@ -33,17 +33,57 @@ included by <termios.h> */
 #define DEFAULT_DEVICEINDEX 0
 #define TIMEOUT_TIME    10 // msec
 
+//  			--- Walking mapping ---
+// 		command				tx#		// RC100 Button
+#define GOFORWARD			(1)		// U
+#define GOBACKWARD			(2) 	// D
+#define TURNLEFT			(4) 	// L
+#define TURNRIGHT			(8) 	// R
+#define GOFORWARDTURNLEFT	(5)		// U+L
+#define GOFORWARDTURNRIGHT	(9)		// U+R
+#define GOLEFT				(260)	// L+5
+#define GORIGHT				(264)	// R+5
+#define GOFORWARDLEFT 		(261)	// L+U+5
+#define GOFORWARDRIGHT 		(265)	// R+U+5
+#define GOBACKWARDLEFT 		(262)	// L+D+5
+#define GOBACKWARDRIGHT		(266)	// R+D+5
 
-#define FORWARD		(1)		// RC100 Button: U
-#define BACKWARD	(2) 	// RC100 Button: D
-#define TURNLEFT	(4) 	// RC100 Button: L
-#define TURNRIGHT	(8) 	// RC100 Button: R
-#define KICK		(16) 	// RC100 Button: 1
-#define command7	(32) 	// RC100 Button: 2
-#define command8	(64) 	// RC100 Button: 3
-#define command9	(128) 	// RC100 Button: 4
-#define command10	(256) 	// RC100 Button: 5
-#define command11	(512) 	// RC100 Button: 6
+//  			--- Kick mapping ---
+// 		command				tx#		// RC100 Button
+#define LEFTKICKFORWARD 	(33)	// 2+U
+#define RIGHTKICKFORWARD 	(129)	// 4+U
+#define LEFTKICKHARD		(545)	// 2+6+U
+#define RIGHTKICKHARD 		(641)	// 4+6+U
+#define LEFTKICKBACKWARD 	(34) 	// 2+D
+#define RIGHTKICKBARDWARD 	(130)	// 4+D
+#define LEFTKICKLEFT 		(36) 	// 2+L
+#define RIGHTKICKLEFT 		(132) 	// 4+L
+#define LEFTKICKRIGHT 		(40) 	// 2+R
+#define RIGHTKICKRIGHT 		(136) 	// 4+R
+
+//  			--- Block mapping ---
+// 		command				tx#		// RC100 Button
+#define PREPAREBLOCK 		(64) 	// 3
+#define BLOCKFRONT 			(65) 	// 3+U
+#define BLOCKLEFT 			(68) 	// 3+L
+#define BLOCKRIGHT 			(72) 	// 3+R
+
+//  			--- Misc mapping ---
+// 		command				tx#		// RC100 Button
+#define TORQUEOFF 			(834) 	// 5+6+D+3
+#define TORQUEON 			(785) 	// 5+6+U+1
+#define STANDUPCHEST 		(17) 	// 1+U
+#define STANDUPBACK 		(18) 	// 1+D
+#define BEATCHEST 			(20) 	// 1+L
+#define CHEER 				(24) 	// 1+R
+
+// 				--- reference buttons ---
+#define button1				(16) 	// 1
+#define button2				(32) 	// 2
+#define button3				(64) 	// 3
+#define button4				(128) 	// 4
+#define button5				(256) 	// 5
+#define button6				(512) 	// 6
 
 
 #ifdef __cplusplus
