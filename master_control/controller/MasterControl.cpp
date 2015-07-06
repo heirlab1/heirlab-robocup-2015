e744ca8ba054fd078e52ceddd24c3c914831f2ac
muhsinking <muhsinking@gmail.com>
2015-07-06 15:57:33 -0500

    create MasterControl skeleton

--- master_control/controller/MasterControl.cpp
+++ master_control/controller/MasterControl.cpp
@@ -1,53 +1,47 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <VisionController.h>
-#include <master_control.h>
+#include <Master_Control.h>
 
 extern "C"{
   #include <tx_serial.h>
 }
 
+
+int MUL8_action = 0;
+int MUL8_state = -1;
+
+int currentThreshold = 85;
+int firstThreshold = 80;
+int secondThreshold = 70;
+
+bool MUL8_initialized = false;
+bool MUL8_ready = false;
+bool MUL8_set = false;
+bool MUL8_play_start = false;
+bool MUL8_finished = false;
+bool second_half = false;
+
 int port;
+VisionController *vc;
+MasterControl *mc;
+
+/* Boolean used for search function */
+bool mul8_knows_position = false;
+
+struct RoboCupGameControlData myData;
+
 
 int main(){
 
   port = open_port();
   // int i;
   // int RxData;
-  int TX_command;
-  VisionController *vc;
-  vc = new VisionController();
+  vc = new Vision();
+  mc = new MasterControl();
 
-	while (1) {
+  while (1) {
     printf("%f\n", vc->getBallAngle());
-
-
-    /*printf("Type in a number \n");
-    
-    if(scanf("%d", &TX_command)){
-      printf("Wrote: %d\n", TX_command);
-
-      if(TX_command == 1){
-        if (tx_data(port, GOFORWARD))
-          fputs("success!\n", stderr);   
-        else
-          fputs("failure :(\n", stderr);
-
-      }
-
-      if(TX_command == 2){
-        if (tx_data(port, GOBACKWARD))
-          fputs("success!\n", stderr);
-        else
-          fputs("failure :(\n", stderr);
-      }
-    }
-
-    else {
-      printf("That ain't a number ya donkus");
-      break;    
-    }*/
-    
   }
 
   return 0;
@@ -58,4 +52,57 @@ int execute_motion(int command){
     fputs("success!\n", stderr);   
   else
     fputs("failure :(\n", stderr);
+}
+
+double MasterControl::getUnixTime(){
+
+}
+
+// action methods
+void MasterControl::search(){
+
+}
+
+void MasterControl::walkTowardsBall(){
+  execute_motion(GOFORWARD);
+}
+
+void MasterControl::getBehindBall(){
+
+}
+
+void MasterControl::alignToKick(){
+
+}
+
+
+// state methods
+
+void MasterControl::init(){
+
+}
+
+void MasterControl::read(){
+
+}
+
+void MasterControl::play(){
+
+}
+
+void MasterControl::finish(){
+
+}
+
+void MasterControl::penalty(){
+
+}
+
+
+MasterControl::MasterControl(){
+
+}
+
+MasterControl::~MasterControl(){
+  
 }
